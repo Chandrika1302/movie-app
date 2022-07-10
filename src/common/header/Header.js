3638a4656dbaee9481b51182b33741bf41ed4024
chandrika <gehini1302@gmail.com>
2022-07-10 17:20:27 +0530

    Added ‘Login’ button on Header

---
+++ src/common/header/Header.js
@@ -0,0 +1,10 @@
+import {Component} from 'react';
+import { Button } from '@material-ui/core';
+class Header extends Component{
+    render(){
+        return(
+            <Button variant='contained' color='default'>Login</Button>
+        )
+    }
+}
+export default Header;
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,8 +1,11 @@
 import {Component} from 'react';
+import Header from '../../common/header/Header';
 class Home extends Component{
     render(){
         return(
-            <div>Hi</div>
+            <div>
+                <Header></Header>
+            </div>
         )
     }
 }
