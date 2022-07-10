c158f09b7c7ff52c7d2799c51722f18b4762a607
chandrika <gehini1302@gmail.com>
2022-07-10 17:49:35 +0530

    added modal for loin button

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,9 +1,31 @@
 import {Component} from 'react';
 import { Button } from '@material-ui/core';
+import SlideshowIcon from '@mui/icons-material/Slideshow';
+import Modal from 'react-modal';
+import './Header.css'
 class Header extends Component{
+    constructor(){
+        super();
+        this.state={
+            modalIsOpen:false
+        }
+    }
+    openModalHandler = () => {
+        this.setState({modalIsOpen:true})
+    }
+    closeModalHandler = () => {
+        this.setState({modalIsOpen:false})
+    }
+
     render(){
         return(
-            <Button variant='contained' color='default'>Login</Button>
+            <div> 
+            <header className='app-header'>
+            <SlideshowIcon className='header-icon'></SlideshowIcon>
+            <Button variant='contained' color='default' className='login-button' onClick={this.openModalHandler}>Login</Button>
+            </header>
+            <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} onRequestClose={this.closeModalHandler}></Modal>
+            </div>
         )
     }
 }
